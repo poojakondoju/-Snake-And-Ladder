9dacad9f625cbdc93b095a4911f6496a33ef84a2
poojakondoju <kondojupoojitha612@gmail.com>
2021-08-23 22:42:21 +0530

    Added:Initial Position Added

---
+++ Snakeandladder.java
@@ -0,0 +1,11 @@
+package snakeandladder;
+
+public class Snakeandladder {
+	public static void main(String[] args) {
+		System.out.println("Welcome To Snake And Ladder");
+
+         int startPosition=0;
+		}
+			
+	}
+

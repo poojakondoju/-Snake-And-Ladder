dbe779b953dedc4b938faa618651ebe45451eb5a
poojakondoju <kondojupoojitha612@gmail.com>
2021-08-23 22:58:14 +0530

    Added:UC3 Player Position Added

--- Snakeandladder.java
+++ Snakeandladder.java
@@ -1,12 +1,33 @@
 package snakeandladder;
 
 public class Snakeandladder {
-	public static void main(String[] args) {
-		System.out.println("Welcome To Snake And Ladder");
+	public static final int NO_PLAY = 0;
+    public static final int LADDER = 1;
+    public static final int SNAKE = 2;
 
-         int startPosition=0;
-         int DieRolled = (int) (Math.random()*6)+1;
-         System.out.println(DieRolled);
-		}
-			
-	}
+public static void main(String[] args) {
+	System.out.println("Welcome To Snake And Ladder");
+
+	 int Position=0;
+     int newPosition=0;
+
+     int DieRolled = (int) (Math.random()*6)+1;
+     int checkOption = (int) (Math.random()*3);
+
+     switch (checkOption){
+         case NO_PLAY:
+               newPosition = 0;
+                break;
+         case LADDER:
+             newPosition = DieRolled;
+               break;
+         case SNAKE:
+             newPosition = DieRolled;
+     }
+     Position=Position+newPosition;
+     if (Position<0){
+              Position = 0;
+     }
+     System.out.println("You are now at "+Position+"th"+" position");
+}
+}
